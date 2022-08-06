2f12792ee148632243f5377d50a03f6a1a42483e
lilianjau <Lilian Jau>
2022-08-06 13:44:51 +0200

    Use Logback

--- src/main/java/com/lilianj91/appcontact/contact/ContactController.java
+++ src/main/java/com/lilianj91/appcontact/contact/ContactController.java
@@ -1,5 +1,7 @@
 package com.lilianj91.appcontact.contact;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("contacts")
 class ContactController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ContactController.class);
+
     @GetMapping("/")
     Contact getContact() {
+
+        LOGGER.info("Contact recovered");
+
         return new Contact(
                 "firstName",
                 "lastName",
